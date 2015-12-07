8ee4ef81a7e5bbb8d2ffbadc6125e45e61b09b12
Kyle <kw15469@my.bristol.ac.uk>
2015-12-07 02:07:46 +0000

    added some sort of interpret function

--- heap.c
+++ heap.c
@@ -4,11 +4,11 @@
 
 // Allocates space given a gap (current item in ring), and size of gap needed
 // It will split the gap accordingly if needed.
-void allocSpace(int size, ring *r){
+void allocSpace(ring *r, int size, int index){
     // item {int index, size; bool used};
     item current = get(r);
-    item newUsed = {current.index, size, true};
-    item newGap = {current.index+1, current.size - size, false};
+    item newUsed = {index, size, true};
+    item newGap = {-1, current.size - size, false};
     removeItem(r);
     addItem(r, newUsed);
     next(r);
@@ -27,13 +27,28 @@ bool searchGap(ring *r, int size){
     return false;
 }
 
+// Runs operation to allocate/free space in heap
+// operation 0 = allocate space
+// operation 1 = free space
+// Size only applicable if allocating, set to -1 if freeing
+void runOperation(ring *r, int operation, int index, int size){
+    if(operation == 0){
+        if(searchGap(r, size) == true) allocSpace(r, size, index);
+        else printf("Error: Ran out of memory\n" );
+    }
+    else if(operation == 1){
+        //TODO: Define search and freeSpace.
+        if(search(r, index) == true) freeSpace(r, index);
+    }
+}
+
 int main(){
     ring *heap = newRing();
     // newHeap = {first index, size of heap, space not allocated so false}
-    item newHeap = {0, 128, false};
+    item newHeap = {-1 , 128, false};
     addItem(heap, newHeap);
     start(heap);
-    allocSpace(10, heap);
+    allocSpace(heap, 10, 0);
     start(heap);
     printf("%d %d %d\n", get(heap).index, get(heap).size, get(heap).used);
     next(heap);
